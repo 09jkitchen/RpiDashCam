46a726039628ef583d447b58f008b425c253f229
09jkitchen <09jkitchen@live.com>
2017-01-07 03:47:00 -0700

    Update dash_cam2.py
    functionality update w/ better raspicam writing. defaults to on at startup.

--- dash_cam2.py
+++ dash_cam2.py
@@ -9,6 +9,7 @@ from subprocess import call #call external commands
 import time
 import os.path
 import gps
+import os
 
 GPIO.setmode(GPIO.BCM)
 LED = 4
@@ -20,59 +21,66 @@ GPIO.setup(Btn2, GPIO.IN, GPIO.PUD_UP)
 loc = '/home/pi/Desktop/video_'
 overlay = "null";
 
+#start up gpsd
+command = "sudo killall gpsd"
+p = os.system(command)
+time.sleep(2) #give it a sec to kill old gpsd 
+command = "sudo gpsd -n /dev/serial0 -F /var/run/gpsd.sock"
+p = os.system(command)
 #Listen to gps on gpsd
 session = gps.gps("localhost", "2947")
 session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
+camera = picamera.PiCamera()
+camera.resolution = (1280, 720)
+camera.framerate = 25
+camera.hflip = True
+camera.vflip = True
+stream = picamera.PiCameraCircularIO(camera, seconds=60)
+camera.start_recording(stream, format='h264')
+
+GPIO.add_event_detect(Btn1, GPIO.FALLING, callback=Btn1_callback, bouncetime=500)
+GPIO.add_event_detect(Btn2, GPIO.FALLING, callback=Btn2_callback, bouncetime=500)
+recording=False
+end=False
+
+start_time = 0
+end_time = 0
 
 def Btn1_callback(pin):
     global started
+	global start_time
+	start_time = time.time()
     started = not started
     print('Button 1 pushed')
     print(started)
 
 def Btn2_callback(pin):
-    global recording
-    recording = not recording
+    global end
     print('Button 2 pushed')
-    print(recording)
-    if (recording):
-        GPIO.output(LED, GPIO.HIGH)
-    else:
-        GPIO.output(LED, GPIO.LOW)
-    
+    end = True
 
 def write_now():
-    global recording
-    if recording:
+    global start_time
+	time_now = time.time()
+    if ((time_now - start_time) > 10): 
         return True
     else:
         return False
 
 def write_video(stream):
     global recording
+	global loc
     print('Writing video!')
-    with stream.lock:
-        # Find the first header frame in the video
-        for frame in stream.frames:
-            if frame.frame_type == picamera.PiVideoFrameType.sps_header:
-                stream.seek(frame.position)
-                break
-        # Write the rest of the stream to disk
-        filename = loc + datetime.now().strftime("%B_%d_%s")
-        with io.open(filename + '.h264', 'wb') as output:
-            output.write(stream.read())
-        print('Done writing video')
-        recording = False
-        GPIO.output(LED, GPIO.LOW)
-        while (not os.path.isfile(filename + ".h264")):
-            time.sleep(0.5)
+	filename = loc + datetime.now().strftime("%B_%d_%s")
+	stream.copy_to(filename + '.h264') #Write video to file
+	print('Done Writing Video')
             #Loop until file is made
-        try:
-            cmd="ffmpeg -r 25 -i " + filename + ".h264 -vcodec copy " + filename + ".mp4"
-            call([cmd])
-            print('Made .mp4')
-        except:
-            print('MP4 failed to create')
+        #try:
+            #cmd="ffmpeg -r 25 -i " + filename + ".h264 -vcodec copy " + filename + ".mp4"
+            #call([cmd])
+            #print('Made .mp4')
+        #except:
+            #print('MP4 failed to create')
 
 def loop():
     GPIO.output(LED, GPIO.HIGH)
@@ -92,7 +100,7 @@ def get_time():
         if hasattr(report, 'time'):
             overlay = report.time
             if hasattr(report, 'speed'):
-                overlay += (" " + str(report.speed * 1.6))
+                overlay += (" " + str(report.speed) + "mph")
                 if hasattr(report, 'lat'):
                     overlay += (" " + str(report.lat))
                     if hasattr(report, 'lon'):
@@ -102,30 +110,11 @@ def get_time():
     #curr_time = datetime.now().strftime("%B_%d_%s")
     return overlay
 
-GPIO.add_event_detect(Btn1, GPIO.FALLING, callback=Btn1_callback, bouncetime=500)
-GPIO.add_event_detect(Btn2, GPIO.FALLING, callback=Btn2_callback, bouncetime=500)
-recording=False
-started=False
-
 while True:
-    with picamera.PiCamera() as camera:
-        camera.resolution = (1280, 720)
-        camera.framerate = 25
-        camera.hflip = True
-        camera.vflip = True
-        stream = picamera.PiCameraCircularIO(camera, seconds=60)
-        camera.start_recording(stream, format='h264')
-        try:
-            while started:
-                curr_time = get_time()
-                camera.annotate_text = curr_time
-                camera.wait_recording(1)
-                if write_now():
-                    # Keep recording for 10 seconds and only then write the
-                    # stream to disk
-                    print('Start Recording')
-                    camera.wait_recording(5)
-                    write_video(stream)
-        finally:
+   	curr_time = get_time() #1Hz update rate
+	camera.annotate_text = curr_time
+		if write_now():
+			print('Start Recording')
+			write_video(stream)
+        if end:
             camera.stop_recording()
-            loop()
